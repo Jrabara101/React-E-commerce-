089d06510538ed64a344da272a7fc43c3f04f429
Jrabara101 <jrabara101@gmail.com>
2025-06-17 11:23:37 +0800

    modified

--- Project2/cities.js
+++ Project2/cities.js
@@ -1,5 +1,44 @@
 const regionCities = {
-  "NCR": ["Manila, PH", "Quezon City, PH", "Pasig, PH"],
-  "Region IV-A": ["Calamba, PH", "Antipolo, PH", "Batangas, PH"],
-  "Region VII": ["Cebu, PH", "Lapu-Lapu City, PH", "Mandaue, PH"]
+  "NCR": [
+    "Manila,PH", "Quezon City,PH", "Caloocan,PH", "Las Pinas,PH", "Makati,PH", "Malabon,PH", "Mandaluyong,PH", "Marikina,PH", "Muntinlupa,PH", "Navotas,PH", "Paranaque,PH", "Pasay,PH", "Pasig,PH", "Pateros,PH", "San Juan,PH", "Taguig,PH", "Valenzuela,PH"
+  ],
+  "Region I": [
+    "San Fernando,La Union,PH", "Laoag,PH", "Vigan,PH", "Dagupan,PH"
+  ],
+  "Region II": [
+    "Tuguegarao,PH", "Cauayan,PH", "Ilagan,PH", "Santiago,PH"
+  ],
+  "Region III": [
+    "San Fernando,Pampanga,PH", "Angeles,PH", "Balanga,PH", "Cabanatuan,PH", "Gapan,PH", "Malolos,PH", "Meycauayan,PH", "Olongapo,PH", "Palayan,PH", "San Jose del Monte,PH", "Tarlac,PH"
+  ],
+  "Region IV-A": [
+    "Calamba,PH", "Antipolo,PH", "Batangas,PH", "Cavite,PH", "Lucena,PH", "San Pablo,PH", "Santa Rosa,PH", "Tanauan,PH", "Trece Martires,PH"
+  ],
+  "Region IV-B": [
+    "Calapan,PH", "Puerto Princesa,PH"
+  ],
+  "Region V": [
+    "Legazpi,PH", "Masbate,PH", "Naga,PH", "Sorsogon,PH"
+  ],
+  "Region VI": [
+    "Iloilo,PH", "Bacolod,PH", "Roxas,PH", "Kabankalan,PH", "Bago,PH", "Himamaylan,PH", "Talisay,PH"
+  ],
+  "Region VII": [
+    "Cebu,PH", "Lapu-Lapu,PH", "Mandaue,PH", "Tagbilaran,PH", "Toledo,PH", "Dumaguete,PH"
+  ],
+  "Region VIII": [
+    "Tacloban,PH", "Ormoc,PH", "Borongan,PH", "Calbayog,PH", "Catbalogan,PH", "Maasin,PH"
+  ],
+  "Region IX": [
+    "Zamboanga,PH", "Dapitan,PH", "Dipolog,PH", "Pagadian,PH"
+  ],
+  "Region X": [
+    "Cagayan de Oro,PH", "Gingoog,PH", "Iligan,PH", "Valencia,PH"
+  ],
+  "Region XI": [
+    "Davao,PH", "Digos,PH", "Mati,PH", "Panabo,PH", "Tagum,PH"
+  ],
+  "Region XII": [
+    "General Santos,PH", "Koronadal,PH", "Kidapawan,PH", "Tacurong,PH", "Cotabato,PH"
+  ]
 };
