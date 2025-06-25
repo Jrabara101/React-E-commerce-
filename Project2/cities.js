82614800fdadd9adb64ef0b44cf25f829945f033
Johnrick Rabara <158558094+Jrabara101@users.noreply.github.com>
2025-06-25 17:15:27 +0800

    Merge branch 'main' of

--- Project2/cities.js
+++ Project2/cities.js
@@ -1,5 +1,19 @@
 const regionCities = {
-  "NCR": ["Manila, PH", "Quezon City, PH", "Pasig, PH"],
-  "Region IV-A": ["Calamba, PH", "Antipolo, PH", "Batangas, PH"],
-  "Region VII": ["Cebu, PH", "Lapu-Lapu City, PH", "Mandaue, PH"]
+  "NCR": ["Manila, PH", "Quezon City, PH", "Pasig, PH", "Makati, PH", "Taguig, PH"],
+  "Region I - Ilocos Region": ["San Fernando, PH", "Laoag, PH", "Vigan, PH", "Alaminos, PH"],
+  "Region II - Cagayan Valley": ["Tuguegarao, PH", "Ilagan, PH", "Cauayan, PH", "Santiago, PH"],
+  "Region III - Central Luzon": ["San Fernando, PH", "Angeles, PH", "Olongapo, PH", "Balanga, PH"],
+  "Region IV-A": ["Calamba, PH", "Antipolo, PH", "Batangas, PH", "Lucena, PH", "Trece Martires, PH"],
+  "Region IV-B - MIMAROPA": ["Calapan, PH", "Puerto Princesa, PH", "Mamburao, PH", "Romblon, PH"],
+  "Region V - Bicol Region": ["Legazpi, PH", "Naga, PH", "Sorsogon, PH", "Iriga, PH"],
+  "Region VI - Western Visayas": ["Iloilo City, PH", "Bacolod, PH", "Roxas City, PH", "Kalibo, PH"],
+  "Region VII": ["Cebu, PH", "Lapu-Lapu City, PH", "Mandaue, PH", "Tagbilaran, PH", "Dumaguete, PH"],
+  "Region VIII - Eastern Visayas": ["Tacloban, PH", "Ormoc, PH", "Catbalogan, PH", "Borongan, PH"],
+  "Region IX - Zamboanga Peninsula": ["Pagadian, PH", "Zamboanga City, PH", "Dipolog, PH", "Ipil, PH"],
+  "Region X - Northern Mindanao": ["Cagayan de Oro, PH", "Iligan, PH", "Malaybalay, PH", "Oroquieta, PH"],
+  "Region XI - Davao Region": ["Davao City, PH", "Tagum, PH", "Digos, PH", "Mati, PH"],
+  "Region XII - SOCCSKSARGEN": ["Koronadal, PH", "General Santos, PH", "Kidapawan, PH", "Tacurong, PH"],
+  "Region XIII - Caraga": ["Butuan, PH", "Surigao City, PH", "Tandag, PH", "Bayugan, PH"],
+  "BARMM": ["Cotabato City, PH", "Marawi, PH", "Lamitan, PH", "Bongao, PH"],
+  "CAR": ["Baguio, PH", "Tabuk, PH", "Bangued, PH", "La Trinidad, PH"]
 };
