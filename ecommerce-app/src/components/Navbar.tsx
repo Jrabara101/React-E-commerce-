27e896e89acaf3fd30c77c314980f7bc8ae9d002
Johnrick Rabara <158558094+Jrabara101@users.noreply.github.com>
2025-09-04 08:22:30 +0800

    Added Navbar Component

--- ecommerce-app/src/components/Navbar.tsx
+++ ecommerce-app/src/components/Navbar.tsx
@@ -1,6 +1,18 @@
-//import { Link } from 'react-router-dom';
+// ./components/Navbar.tsx
+import React from "react";
+import { Link } from "react-router-dom";
 
-export default function Navbar() {
-  return 
+const Navbar: React.FC = () => {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/products">Products</Link></li>
+        <li><Link to="/cart">Cart</Link></li>
+        <li><Link to="/sale">Sale</Link></li>
+      </ul>
+    </nav>
+  );
+};
 
-}
+export default Navbar;
