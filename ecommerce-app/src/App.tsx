40b7892d9653f236c3a5ebd3ee1c4dcc4bfc5424
Johnrick Rabara <158558094+Jrabara101@users.noreply.github.com>
2025-09-04 21:02:05 +0800

    Added AuthContext, CartContext

--- ecommerce-app/src/App.tsx
+++ ecommerce-app/src/App.tsx
@@ -1,34 +1,37 @@
-import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import Products from './pages/Products';
-import Cart from './pages/Cart';
-import Login from './pages/Login';
-import Sale from './pages/Sale';
+import { BrowserRouter as Router, Routes, Route, Outlet } from "react-router-dom";
+import { CartProvider } from "./context/CartContext";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import Sale from "./pages/Sale";
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <Routes>
-        {/* Auth routes without Navbar */}
-        <Route path="/login" element={<Login />} />
+    <CartProvider>
+      <Router>
+        <Routes>
+          {/* Auth routes without Navbar */}
+          <Route path="/login" element={<Login />} />
 
-        {/* Routes with shared Navbar */}
-        <Route
-          element={
-            <>
-              <Navbar />
-              <Outlet />
-            </>
-          }
-        >
-          <Route path="/" element={<Home />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/sale" element={<Sale />} />
-        </Route>
-      </Routes>
-    </Router>
+          {/* Routes with shared Navbar */}
+          <Route
+            element={
+              <>
+                <Navbar />
+                <Outlet />
+              </>
+            }
+          >
+            <Route path="/" element={<Home />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/sale" element={<Sale />} />
+          </Route>
+        </Routes>
+      </Router>
+    </CartProvider>
   );
 };
 
